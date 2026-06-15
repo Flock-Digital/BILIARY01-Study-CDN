b079fa239a4c558a106d42de6e954805a68642a9
Flock Digital <162156388+FlockDigitalLtd@users.noreply.github.com>
2026-06-15 09:59:37 +0100

    Update and rename translations-1.1.3.js to translations-1.1.4.js

--- translations-1.1.4.js
+++ translations-1.1.4.js
@@ -365,30 +365,3 @@ const TRANSLATIONS = {
 		}
 	}
 };
-
-const MULTI_LANGUAGE_COUNTRIES = ['in', 'us', 'ca', 'my', 'ph', 'sa'];
-
-const COUNTRY_DEFAULTS = {
-	'au': 'english',              // Australia
-	'at': 'german',               // Austria
-	'br': 'brazilian-portuguese', // Brazil
-	'ca': 'english',              // Canada
-	'cz': 'english',              // Czech Republic
-	'de': 'german',               // Germany
-	'hk': 'chinese-hk',           // Hong Kong
-	'in': 'hindi',                // India
-	'it': 'italian',              // Italy
-	'jp': 'japanese',             // Japan
-	'my': 'malay',                // Malaysia
-	'ph': 'tagalog',              // Philippines
-	'pl': 'polish',               // Poland
-	'sa': 'arabic',               // Saudi Arabia
-	'sk': 'english',              // Slovakia
-	'es': 'spanish',              // Spain
-	'tw': 'chinese-tw',           // Taiwan
-	'th': 'thai',                 // Thailand
-	'tr': 'turkish',              // Turkey
-	'gb': 'english',              // United Kingdom
-	'us': 'english-us',           // United States
-	'vn': 'vietnamese',           // Vietnam
-};
