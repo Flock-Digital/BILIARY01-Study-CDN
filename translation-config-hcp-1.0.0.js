942487cfd9d3adcabed685b3a51a803981b6d037
Flock Digital <162156388+FlockDigitalLtd@users.noreply.github.com>
2026-06-15 09:58:33 +0100

    Add translation configuration for multiple countries

---
+++ translation-config-hcp-1.0.0.js
@@ -0,0 +1,18 @@
+const MULTI_LANGUAGE_COUNTRIES = ['in', 'us', 'ca', 'my', 'ph', 'sa'];
+
+const COUNTRY_DEFAULTS = {
+	'au': 'english',              // Australia
+	'at': 'german',               // Austria
+	'br': 'brazilian-portuguese', // Brazil
+	'ca': 'english',              // Canada
+	'cz': 'english',              // Czech Republic
+	'de': 'german',               // Germany
+	'it': 'italian',              // Italy
+	'jp': 'japanese',             // Japan
+	'my': 'english',              // Malaysia
+	'ph': 'english',              // Philippines
+	'pl': 'polish',               // Poland
+	'sa': 'english-us',           // Saudi Arabia
+	'sk': 'english',              // Slovakia
+	'tr': 'turkish',              // Turkey
+};
